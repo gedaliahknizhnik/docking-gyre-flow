5cd1958e16f6e7f4d7b6752c5d08e947aaec9d79
Gedaliah Knizhnik <knizhnik@seas.upenn.edu>
2022-07-21 11:59:18 -0400

    Created flowfield with wind-driven double gyre. Added README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# dockingInGyreFlow
-Simulation of control law for docking Modboats in a gyre flow. 
+# Docking in Gyre-Like Flows
+
+This repository is a sub-project of the [Modboats](https://www.modlabupenn.org/modboats/) project. Our goal is to develop controllers that utilize the energy in flow-like environments to help Modboats dock with each other.
+## Background
+
+Modboats have been shown to be able to ride gyre-like flows with limited information and can demonstrate at least 70% energy savings when doing so[^1]. We know aim to show that we can use this approach to find and dock to other Modboat modules.
+
+[^1]: G. Knizhnik, P. Li, X. Yu and M. A. Hsieh, "Flow-Based Control of Marine Robots in Gyre-Like Environments," 2022 International Conference on Robotics and Automation (ICRA), 2022, pp. 3047-3053, [doi: 10.1109/ICRA46639.2022.9812331](https://ieeexplore.ieee.org/document/9812331).
+
+## Simulation
+
+The goal of this repository in particular is to simulate the controllers we are developing in gyre models and verify that they converge. Usage information and results will be added here as the repository progresses.
+
---
+++ flowfield.py
@@ -0,0 +1,89 @@
+from functools import partial
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class GyreFlow:
+    """Defines a gyre-like flow field that provides velocities for simulation"""
+
+    def __init__(self, *, flow_model: callable, **kwargs) -> None:
+        """
+        Creates a flow_func function with the keyword arguments passed along.
+        We assume that flow_func() takes a set of points and some parameters, and
+        create a partial function that takes only the points.
+
+        Inputs:
+            flow_model: callable function model
+            **kwargs: any parameters for the flow_model.
+
+        Usage:
+            g = GyreFlow(flow_model=SOMEMODEL, param1=1, param2=2, ...)
+            dpts = g.flow_func(pts)
+
+        """
+
+        self.flow_func = partial(double_gyre, **kwargs)
+
+    def plot(self, step: float, lims: np.ndarray) -> None:
+        """
+        Plots a flow-field for the internal flow_func on a single plot.
+
+        Inputs:
+            step: the discretization of the plotted flow-field.
+            lims: 4-long numpy array of plotting limits [xmin, xmax, ymin, ymax]
+        """
+
+        # If no plot exists, create it. Otherwise, reuse it.
+        if not hasattr(self, "f"):
+            self.f, self.ax = plt.subplots()
+
+        x1s = np.arange(lims[0], lims[1], step)
+        x2s = np.arange(lims[2], lims[3], step)
+
+        x1mesh, x2mesh = np.meshgrid(x1s, x2s)
+        dxs = self.flow_func(np.array((x1mesh, x2mesh)))
+
+        self.ax.quiver(x1mesh, x2mesh, dxs[0], dxs[1])
+        plt.draw()
+
+
+def double_gyre(pts: np.ndarray, A: float, s: float, mu: float) -> np.ndarray:
+    """
+    Models the classical wind-driven double-gyre flow model.
+
+    Inputs:
+        pts: numpy array containing the points at which to evaluate.
+             Supports single points, vectors, or meshgrid, assuming that
+             pts[0] contains x1, pts[1] contains x2
+        A: circulation amplitude
+        s: circulation dimension
+        mu: dissipation parameter
+
+    Outputs:
+        dxs: flow-field at each point in pts, with shape matching pts.
+             dxs[0] contains dx1, dxs[1] contains dx2
+    """
+
+    x1s, x2s = pts[0], pts[1]
+
+    factor1 = np.pi * x1s / s
+    factor2 = np.pi * x2s / s
+
+    dx1s = -np.pi * A * np.sin(factor1) * np.cos(factor2) - mu * x1s
+    dx2s = np.pi * A * np.cos(factor1) * np.sin(factor2) - mu * x2s
+
+    dxs = np.array((dx1s, dx2s))
+
+    return dxs
+
+
+def main():
+
+    g = GyreFlow(flow_model=double_gyre, A=1, s=1, mu=1)
+    g.plot(0.1, np.array((-1, 1, -1, 1)))
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
